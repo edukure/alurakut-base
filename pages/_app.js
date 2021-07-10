396933f64940313920b876a4fc244e38ceb95039
Eduardo Tioma <tioma.eduardo@gmail.com>
2021-07-10 15:18:19 -0300

    Aula 01 - Adicionar reset CSS básico

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,38 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
 
-const GlobalStyle = createGlobalStyle`
-  body {
+export const GlobalStyle = createGlobalStyle`
+  * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
   }
-`
+  
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    background-color: #D9E6F6;
+    font-family: sans-serif;
+  }
+
+  #__next {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+    display: block;
+  }
+`;
 
 const theme = {
   colors: {
     primary: '#0070f3',
   },
-}
+};
 
 export default function App({ Component, pageProps }) {
   return (
@@ -22,5 +42,5 @@ export default function App({ Component, pageProps }) {
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
