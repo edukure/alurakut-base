992a64348e1a8836347aca65909c309efc61e243
Eduardo Tioma <tioma.eduardo@gmail.com>
2021-07-10 15:21:45 -0300

    Aula 01 - Criar ProfileSidebar no Home

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
 import Box from '../src/components/Box';
 import MainGrid from '../src/components/MainGrid';
 
+const ProfileSidebar = (propriedades) => {
+  return (
+    <Box>
+      <img src={`https://github.com/${propriedades.githubUser}.png`} style={{ borderRadius: '8px' }} />
+    </Box>
+  );
+};
+
 export default function Home() {
   const githubUser = 'edukure';
 
@@ -8,9 +16,8 @@ export default function Home() {
     <>
       <MainGrid>
         <div className="profileArea" style={{ gridArea: 'profileArea' }}>
-          <Box>
-            <img src={`https://github.com/${githubUser}.png`} style={{ borderRadius: '8px' }} />
-          </Box>
+          {/* <Box><img src={`https://github.com/${githubUser}.png`} style={{borderRadius: "8px"}}/></Box> */}
+          <ProfileSidebar githubUser={githubUser} />
         </div>
         <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
           <Box>Welcome</Box>
