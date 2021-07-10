c3574370ba29eb03f8bffa55caba003ea7b86202
Eduardo Tioma <tioma.eduardo@gmail.com>
2021-07-10 15:21:11 -0300

    Aula 01 - Aplicar MainGrid no Home

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,24 @@
-import styled from 'styled-components'
-
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+import Box from '../src/components/Box';
+import MainGrid from '../src/components/MainGrid';
 
 export default function Home() {
-  return <Title>My page</Title>
+  const githubUser = 'edukure';
+
+  return (
+    <>
+      <MainGrid>
+        <div className="profileArea" style={{ gridArea: 'profileArea' }}>
+          <Box>
+            <img src={`https://github.com/${githubUser}.png`} style={{ borderRadius: '8px' }} />
+          </Box>
+        </div>
+        <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
+          <Box>Welcome</Box>
+        </div>
+        <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
+          <Box>Pessoas da Comunidade</Box>
+        </div>
+      </MainGrid>
+    </>
+  );
 }
