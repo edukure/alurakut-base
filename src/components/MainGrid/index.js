b8ad1a806fe7fd5e154952e53a705ad07633bde2
Eduardo Tioma <tioma.eduardo@gmail.com>
2021-07-10 15:19:05 -0300

    Aula 01 - Criar MainGrid

---
+++ src/components/MainGrid/index.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+const MainGrid = styled.main`
+  width: 100%;
+  max-width: 500px;
+  margin-left: auto;
+  margin-right: auto;
+  grid-gap: 10px;
+  padding: 16px;
+
+  @media (min-width: 860px) {
+    max-width: 1110px;
+    display: grid;
+    grid-template-areas: 'profileArea welcomeArea profileRelationsArea';
+    grid-template-columns: 160px 1fr 312px;
+  }
+
+  .profileArea {
+    display: none;
+
+    @media (min-width: 860px) {
+      display: block;
+    }
+  }
+`;
+
+export default MainGrid;
