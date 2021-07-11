c222efff9dfec3ab686fef886472d53e40263aea
Eduardo Tioma <tioma.eduardo@gmail.com>
2021-07-11 12:01:51 -0300

    Aula 02 - Implementar state/display de comunidades

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import Box from '../src/components/Box';
 import MainGrid from '../src/components/MainGrid';
 import {
@@ -35,6 +37,14 @@ export default function Home() {
     'felipefialho',
   ];
 
+  const [comunidades, setComunidades] = React.useState([
+    {
+      id: '3123213214',
+      title: 'Eu odeio acordar cedo',
+      image: 'https://alurakut.vercel.app/capa-comunidade-01.jpg',
+    },
+  ]);
+
   return (
     <>
       <AlurakutMenu />
@@ -56,6 +66,18 @@ export default function Home() {
             <form
               onSubmit={function handleCriaComunidade(e) {
                 e.preventDefault();
+
+                const dadosDoForm = new FormData(e.target);
+
+                const comunidade = {
+                  id: new Date().toISOString(),
+                  title: dadosDoForm.get('title'),
+                  image: dadosDoForm.get('image'),
+                };
+
+                const comunidadesAtualizadas = [...comunidades, comunidade];
+
+                setComunidades(comunidadesAtualizadas);
               }}>
               <div>
                 <input
@@ -84,10 +106,26 @@ export default function Home() {
             <ul>
               {pessoasFavoritas.map((itemAtual) => {
                 return (
-                  <li>
-                    <a href={`/users/${itemAtual}`} key={itemAtual}>
+                  <li key={itemAtual}>
+                    <a href={`/users/${itemAtual}`}>
                       <img src={`https://github.com/${itemAtual}.png`} />
                       <span>{itemAtual}</span>
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
+
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">Comunidades ({comunidades.length})</h2>
+            <ul>
+              {comunidades.map((itemAtual) => {
+                return (
+                  <li key={itemAtual.id}>
+                    <a href={`/users/${itemAtual.title}`}>
+                      <img src={itemAtual.image} />
+                      <span>{itemAtual.title}</span>
                     </a>
                   </li>
                 );
